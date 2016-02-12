b6cdd16b469fb4f449be57a917cd416ba3dbc59d
Abel Tesfaye <abel-tesfaye@hotmail.com>
2016-02-11 22:02:28 -0500

    drive distance

---
+++ 2016 Baseline/src/org/usfirst/frc/team614/robot/commands/DriveDistance.java
@@ -0,0 +1,68 @@
+package org.usfirst.frc.team614.robot.commands;
+
+import org.usfirst.frc.team614.robot.Robot;
+import org.usfirst.frc.team614.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class DriveDistance extends Command {
+	
+	//Holds the Encoder Value to kill at(+ = Forwards/ - = Backwards)
+	private double distance;
+	private boolean driveForward;
+	
+	//Timeout/Ending Variables
+	private boolean isDone;
+	private double timeout;
+	
+    public DriveDistance(double distance, boolean forward, double time) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drivetrain);
+    	
+    	this.distance = distance;
+    	driveForward = forward;
+    	timeout = time;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.drivetrain.stopDrive();
+    	
+    	if(driveForward){
+    		distance += Robot.drivetrain.getDistanceTravelled();
+    	}else{
+    		distance -= Robot.drivetrain.getDistanceTravelled();
+    	}
+    	setTimeout(timeout);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute(){
+		double motorSpeed = RobotMap.AUTO_MOTOR_MAGNITUDE;//define this
+		if(!driveForward){
+			motorSpeed *= -1.0;
+		}
+    	Robot.drivetrain.arcadeDriveMode(motorSpeed, 0);
+    	if(Math.abs(Robot.drivetrain.getDistanceTravelled()) > distance) {
+    		isDone = true;
+    	}
+    }
+
+
+    protected boolean isFinished() {
+        return isDone || isTimedOut();
+    }
+
+    protected void end(){
+    	Robot.drivetrain.stopDrive();
+    }
+
+    protected void interrupted(){
+    	Robot.drivetrain.stopDrive();
+    }
+}
