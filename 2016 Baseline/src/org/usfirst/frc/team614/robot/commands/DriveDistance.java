c489a02f2d727e2ff57a4d1dc5ed0e8be3014d7b
Abel Tesfaye <abel-tesfaye@hotmail.com>
2016-02-12 19:32:42 -0500

    formatting

--- 2016 Baseline/src/org/usfirst/frc/team614/robot/commands/DriveDistance.java
+++ 2016 Baseline/src/org/usfirst/frc/team614/robot/commands/DriveDistance.java
@@ -10,54 +10,53 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class DriveDistance extends Command {
-	
-	//Holds the Encoder Value to kill at(+ = Forwards/ - = Backwards)
+
+	// Holds the Encoder Value to kill at(+ = Forwards/ - = Backwards)
 	private double distance;
 	private boolean driveForward;
-	
-	//Timeout/Ending Variables
+
+	// Timeout/Ending Variables
 	private boolean isDone;
 	private double timeout;
-	
-    public DriveDistance(double distance, boolean forward, double time) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.drivetrain);
-    	
-    	this.distance = distance;
-    	driveForward = forward;
-    	timeout = time;
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	Robot.drivetrain.stopDrive();
-    	Robot.drivetrain.resetDistance();
-    	setTimeout(timeout);
-    }
+	public DriveDistance(double distance, boolean forward, double time) {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.drivetrain);
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute(){
-		double motorSpeed = RobotMap.AUTO_MOTOR_MAGNITUDE;//define this
-		if(!driveForward){
+		this.distance = distance;
+		driveForward = forward;
+		timeout = time;
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.drivetrain.stopDrive();
+		Robot.drivetrain.resetDistance();
+		setTimeout(timeout);
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double motorSpeed = RobotMap.AUTO_MOTOR_MAGNITUDE;// define this
+		if (!driveForward) {
 			motorSpeed *= -1.0;
 		}
-    	Robot.drivetrain.arcadeDriveMode(motorSpeed, 0);
-    	if(Math.abs(Robot.drivetrain.getDistanceTravelled()) > distance) {
-    		isDone = true;
-    	}
-    }
+		Robot.drivetrain.arcadeDriveMode(motorSpeed, 0);
+		if (Math.abs(Robot.drivetrain.getDistanceTravelled()) > distance) {
+			isDone = true;
+		}
+	}
 
+	protected boolean isFinished() {
+		return isDone || isTimedOut();
+	}
 
-    protected boolean isFinished() {
-        return isDone || isTimedOut();
-    }
+	protected void end() {
+		Robot.drivetrain.stopDrive();
+	}
 
-    protected void end(){
-    	Robot.drivetrain.stopDrive();
-    }
-
-    protected void interrupted(){
-    	Robot.drivetrain.stopDrive();
-    }
+	protected void interrupted() {
+		Robot.drivetrain.stopDrive();
+	}
 }
